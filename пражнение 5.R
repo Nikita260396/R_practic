d5ea26edb2d59cbf4db9eb1646c39ca03eea7f4f
Nikita260396 <71729237+Nikita260396@users.noreply.github.com>
2020-10-16 18:29:15 +0300

    Update пражнение 5.R

--- пражнение 5.R
+++ пражнение 5.R
@@ -1,9 +1,13 @@
+
 set.seed(6)
 x< -rnorm(300,12,2)
 e< -rnorm(300,0,15)
 y<-100-12*x+e
 x
+png("picture.png")
+layout(matrix(c(2,1,0,3),2,2,byrow = T))
 plot(x,y)
 boxplot(x)
-boxplot(y)
+boxplot(y,horizontal = T)
+dev.off()
 
